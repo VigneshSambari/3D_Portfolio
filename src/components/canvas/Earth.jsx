9fc3cf87309db8677e178d78fd4dc7813f608f7d
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2023-08-04 18:50:27 +0530

    contact section

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("./planet_green/scene.gltf");
 
   return (
     <>
